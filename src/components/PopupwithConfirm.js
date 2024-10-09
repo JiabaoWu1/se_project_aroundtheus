157c346f3bdc9e671d0c3a0a0428f4eb9c2b4522
JiabaoWu <wujiabaotonny@gmail.com>
2024-10-09 14:56:09 -0400

    error update

--- src/components/PopupwithConfirm.js
+++ src/components/PopupwithConfirm.js
@@ -3,6 +3,7 @@ import Popup from "./Popup.js";
 export default class PopupWithConfirm extends Popup {
   constructor(popupSelector) {
     super({ popupSelector });
+    this._form = document.querySelector("#trashcan-modal");
   }
 
   setSubmitFunction(submitFunction) {
